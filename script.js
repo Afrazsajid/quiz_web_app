16ae74ddf8df99371ae3edb625d48b4c14349ead
Afrazsajid <101873923+Afrazsajid@users.noreply.github.com>
2024-05-11 17:01:18 +0500

    solving your answer problem

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
-function letterToNumber(letter) {
-    return letter.toLowerCase().charCodeAt(0) - 96;
-  }
 
-  function escapeHtmlTags(str) {
-    return str.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  }
-  const rootStyles = getComputedStyle(document.documentElement);
-  const btn_color = rootStyles.getPropertyValue('--color2')
+
+function numberToLetter(num) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return alphabet[num - 1];
+}
+
+function letterToNumber(letter) {
+  return letter.toLowerCase().charCodeAt(0) - 96;
+}
+
+function escapeHtmlTags(str) {
+  return str.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
+const rootStyles = getComputedStyle(document.documentElement);
+const btn_color = rootStyles.getPropertyValue('--color2')
 
 
 const quizContainer = document.querySelector('.quiz-container');
@@ -17,180 +24,196 @@ const nextButton = document.querySelector('#next-button');
 const backButton = document.querySelector('#back-button');
 const resultsContainer = document.querySelector('.results-container');
 const resultsList = document.querySelector('#results-list');
-const section=document.querySelector("#result_cont_down")
+const section = document.querySelector("#result_cont_down")
 
 // API data example
-let quizData 
-  
-  let currentQuestion = 0;
-  let score = 0;
-  let timer = 10; // 10 minutes in seconds
+let quizData
 
-  let slected_opt_id_lst=[]
-  
+let currentQuestion = 0;
+let score = 0;
+let timer = 100; // 10 minutes in seconds
 
-  async function fetchQuestionData() {
-    fetch("https://quizapi.io/api/v1/questions?apiKey=lV4E5MUmNEJi0wLlsoE1mupL1d84QeVUuYQcXni9&difficulty=Medium&limit=7&tags=HTML")
+let slected_opt_id_lst = []
+
+
+async function fetchQuestionData() {
+  fetch("https://quizapi.io/api/v1/questions?apiKey=lV4E5MUmNEJi0wLlsoE1mupL1d84QeVUuYQcXni9&difficulty=Medium&limit=12&tags=HTML")
     .then(response => response.json())
     .then(data => {
 
-      
+
       quizData = data;
       renderQuestion()
-    //   onsole.log(quizData);
-      ;
+        //   onsole.log(quizData);
+        ;
     })
     .catch(error => {
       console.error('Error:', error);
     });
-    
-  }
-  
 
-function renderQuestion(attepmt=false) {
+}
 
 
-    const question = quizData[currentQuestion];
-    questionText.textContent = question.question;
-    options.forEach((option, index) => {
-        option.classList.remove('clicked')
+function renderQuestion(attepmt = false) {
 
-      option.textContent =(Object.values(question.answers)[index]);
+
+  const question = quizData[currentQuestion];
+  questionText.textContent = question.question;
+  options.forEach((option, index) => {
+    option.classList.remove('clicked')
+
+    option.textContent = (Object.values(question.answers)[index]);
     //   console.log(Object.values(question.answers)[index])
-      option.id = `answer_${index + 1}`; // Set the ID of the option button
-      
-      ;
-    });
+    option.id = `answer_${index + 1}`; // Set the ID of the option button
 
-  }
-  // Function to handle option selection
-  function handleOptionSelection(event) {
-    slected_opt_id_lst[currentQuestion]=(event.target.id)
-    renderQuestion()
-    event.target.classList.toggle("clicked")
+    ;
+  });
+
+}
+// Function to handle option selection
+function handleOptionSelection(event) {
+  slected_opt_id_lst[currentQuestion] = (event.target.id)
+  renderQuestion()
+  event.target.classList.toggle("clicked")
     // console.log(currentQuestion)
-    
+
     // console.log(slected_opt_id_lst)
-    
- 
+
+
 
 
 
     ;
-  }
+}
 
-  function renderResults() {
+function renderResults() {
 
-    section.scrollIntoView({ behavior: 'smooth' })
-    const resultsHTML = quizData.map((question, index) => {
-    const cor_ans_ind=question.correct_answer
+  section.scrollIntoView({ behavior: 'smooth' })
+  const resultsHTML = quizData.map((question, index) => {
+    const cor_ans_ind = question.correct_answer
     const correctAnswer = 'answer_' + letterToNumber(question.correct_answer.split('_')[1]);
-      const userAnswer = slected_opt_id_lst[index];
-      const resultClass = userAnswer === correctAnswer ? "green" : "red";
-      return `
+    let userAnswer = slected_opt_id_lst[index];
+    console.log(userAnswer)
+    
+    const resultClass = userAnswer === correctAnswer ? "green" : "red";
+    userAnswer = userAnswer.replace(/_(\d+)/, function(match, number) {
+      return "_" + numberToLetter(parseInt(number));
+    });
+    
+    return `
         <div class="result-item ${resultClass}">
           <>Question :<code>${escapeHtmlTags(question.question)}</code></p>
-          <>Your answer: <code>${escapeHtmlTags(document.getElementById(userAnswer).textContent)}</code></p>
+          <>Your answer: <code>${escapeHtmlTags(question.answers[userAnswer])}</code></p>
           <>Correct answer: <code>${escapeHtmlTags(question.answers[cor_ans_ind])}</code></p>
         </div>
       `;
-    }).join("");
-    resultsList.innerHTML = resultsHTML;
-    document.getElementById('restart').addEventListener('click', function(e) {
-        // console.log("j")
-    
-    
-    
-   
-        window.location.reload(); 
-      });
-  }
-  
+  }).join("");
+  resultsList.innerHTML = resultsHTML;
+  document.getElementById('restart').addEventListener('click', function (e) {
+    // console.log("j")
 
 
 
-  function handleNextButtonClick(e) {
-    // console.log("h")
-    if ((currentQuestion < quizData.length - 1) && (slected_opt_id_lst.length>currentQuestion)){
-      currentQuestion++;
-      renderQuestion()
-    
-    } else if(currentQuestion === quizData.length-1 ) {
-        
-        document.querySelector(".results-container").classList.remove("no")
-        
-      renderResults();
-    }
 
-    if (currentQuestion > quizData.length - 2){
-        e.target.textContent="check result"
-    }
-  }
-  
+    window.location.reload();
+  });
+}
 
-  function handleBackButtonClick() {
-    if (currentQuestion > 0) {
-      currentQuestion--;
-      renderQuestion();
-    }
+
+
+
+function handleNextButtonClick(e) {
+  // console.log("h")
+  if ((currentQuestion < quizData.length - 1) && (slected_opt_id_lst.length > currentQuestion)) {
+    currentQuestion++;
+    renderQuestion()
+
+  } else if (currentQuestion === quizData.length - 1) {
+
+    document.querySelector(".results-container").classList.remove("no")
+
+    renderResults();
   }
 
+  if (currentQuestion > quizData.length - 2) {
+    e.target.textContent = "check result"
+  }
+}
 
-  
 
-  function startTimer() {
-    setInterval(() => {
+function handleBackButtonClick() {
+  if (currentQuestion > 0) {
+    currentQuestion--;
+    renderQuestion();
+  }
+}
+
+
+
+
+function startTimer() {
+  setInterval(() => {
+
+     if (timer > 0) { 
       timer--;
-      const minutes = Math.floor(timer / 60);
-      const seconds = timer % 60;
-      document.querySelector("#timer").textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
-      if (timer === 0) {
-        Swal.fire({
-        
-          html: `<img src="https://c.tenor.com/1ApT-pZWryIAAAAC/tenor.gif" alt="Custom icon">`,
-          title: "Time out",
-          
-          confirmButtonText: 'Try Again',
-          confirmButtonColor: btn_color,
+    }else {
+      clearInterval(intervalId); // Clear the interval when timer is 0
+    }
+   
+    const minutes = Math.floor(timer / 60);
+    const seconds = timer % 60;
+    document.querySelector("#timer").textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`
 
- 
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.reload()
-            
-            
-          } 
-        });
+    if (timer === 0) {
+      Swal.fire({
 
+        html: `<img src="https://c.tenor.com/1ApT-pZWryIAAAAC/tenor.gif" alt="Custom icon">`,
+        title: "Time out",
+
+        confirmButtonText: 'Try Again',
+        confirmButtonColor: btn_color,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+
+
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.reload()
+
+
+        }
+      });
+      clearInterval(intervalId)
         ;
-      }
-    }, 1000);
-  }
-  
-  // Initialize quiz
-  async function restart() {
-    // console.log("h")
-    
-      const data = await fetchQuestionData();
-      startTimer()
-      if (data && data.length > 0) { 
-        quizData = data;
-        // console.log(quizData); // Fix the typo here
-        renderQuestion();
-        ;
-      } else {
-        console.error('No data available');
-      }
+    }
 
-    
-    
-    // Add event listeners
-    options.forEach((option) => {
-      option.addEventListener("click", handleOptionSelection);
-    });
-    nextButton.addEventListener("click", handleNextButtonClick);
-    backButton.addEventListener("click", handleBackButtonClick);
+  }, 1000);
+}
+
+// Initialize quiz
+async function restart() {
+  // console.log("h")
+
+  const data = await fetchQuestionData();
+  startTimer()
+  if (data && data.length > 0) {
+    quizData = data;
+    // console.log(quizData); // Fix the typo here
+    renderQuestion();
+    ;
+  } else {
+    console.error('No data available');
   }
+
+
+
+  // Add event listeners
+  options.forEach((option) => {
+    option.addEventListener("click", handleOptionSelection);
+  });
+  nextButton.addEventListener("click", handleNextButtonClick);
+  backButton.addEventListener("click", handleBackButtonClick);
+}
 
 restart()
 
